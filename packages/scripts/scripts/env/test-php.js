825c98541979476e4f32be4afe68c96fb1fc7b28
Noah Allen <noahtallen@gmail.com>
2020-02-07 02:01:20 -0800

    Use wp-env docker config within test-php

--- packages/scripts/scripts/env/test-php.js
+++ packages/scripts/scripts/env/test-php.js
@@ -2,6 +2,9 @@
  * Node dependencies.
  */
 const { execSync } = require( 'child_process' );
+const path = require( 'path' );
+const os = require( 'os' );
+const crypto = require( 'crypto' );
 const { env } = require( 'process' );
 
 /**
@@ -11,11 +14,33 @@ const { getArgsFromCLI } = require( '../../utils' );
 
 const args = getArgsFromCLI();
 
+// Plugin against which to run the tests.
+const pluginDir = env.npm_package_wp_env_plugin_dir;
 const localDir = env.LOCAL_DIR || 'src';
 
-// Run PHPUnit with the working directory set correctly.
+// Runs the phpunit command in the phpunit Docker service,
 execSync(
-	`npm run test:php -- -c /var/www/${ localDir }/wp-content/plugins/${ env.npm_package_wp_env_plugin_dir }/phpunit.xml.dist ` +
+	`docker-compose run --rm phpunit phpunit -c /var/www/${ localDir }/wp-content/plugins/${ pluginDir }/phpunit.xml.dist` +
 		args.join( ' ' ),
-	{ cwd: env.WP_DEVELOP_DIR, stdio: 'inherit' }
+	{
+		cwd: getDockerComposeDir(),
+		stdio: 'inherit',
+	}
 );
+
+/**
+ * Returns the directory containing the docker-compose file for our .wp-env file.
+ *
+ * Assumes the .wp-env.json file exists in cwd.
+ *
+ * Based on packages/env/lib/config.js.
+ */
+function getDockerComposeDir() {
+	const configPath = path.resolve( '.wp-env.json' );
+	const pathHash = crypto
+		.createHash( 'md5' )
+		.update( configPath )
+		.digest( 'hex' );
+
+	return path.resolve( os.homedir(), '.wp-env', pathHash );
+}
