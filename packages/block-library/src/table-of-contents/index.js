9fac38874f39344e3b1e675b9112197560696bed
Zebulan Stanphill <zebulanstanphill@protonmail.com>
2020-04-18 12:52:32 -0500

    General block polish.

--- packages/block-library/src/table-of-contents/index.js
+++ packages/block-library/src/table-of-contents/index.js
@@ -20,7 +20,10 @@ export const settings = {
 		'Summarize your post with a list of headings. Add HTML anchors to Heading blocks to link them here.'
 	),
 	icon: 'list-view',
-	category: 'layout',
+	supports: {
+		html: false,
+		lightBlockWrapper: true,
+	},
 	transforms,
 	edit,
 };
