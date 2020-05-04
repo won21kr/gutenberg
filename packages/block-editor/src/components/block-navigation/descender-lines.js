d1ac0e7901dcca9e708d53566c61671618e247b7
Daniel Richards <daniel.richards@automattic.com>
2020-05-04 17:27:33 +0800

    Remove descender lines from the accessibility tree

--- packages/block-editor/src/components/block-navigation/descender-lines.js
+++ packages/block-editor/src/components/block-navigation/descender-lines.js
@@ -12,13 +12,14 @@ export default function DescenderLines( {
 	terminatedLevels,
 } ) {
 	return times( level - 1, ( index ) => {
-		// Add 2, 1 because there's no descender for the first level, and
-		// another 1 because levels start at 1, which indices start at 0.
+		// The first 'level' that has a descender line is level 2.
+		// Add 2 to the zero-based index below to reflect that.
 		const currentLevel = index + 2;
 		const hasItem = currentLevel === level;
 		return (
 			<div
 				key={ index }
+				aria-hidden="true"
 				className={ classnames( lineClassName, {
 					'has-item': hasItem,
 					'is-last-row': isLastRow,
