91658de1529de45a01fd407b1a90d33e17fe3afc
Zebulan Stanphill <zebulanstanphill@protonmail.com>
2020-05-05 13:47:20 -0500

    Optimize check for nullish rootChildBlocks.

--- packages/block-editor/src/components/inserter/block-list.js
+++ packages/block-editor/src/components/inserter/block-list.js
@@ -110,9 +110,11 @@ function InserterBlockList( {
 	}, [ filterValue, items, categories, collections ] );
 
 	const childItems = useMemo( () => {
-		return filter( filteredItems, ( { name } ) =>
-			rootChildBlocks?.includes( name )
-		);
+		return rootChildBlocks
+			? filter( filteredItems, ( { name } ) =>
+					rootChildBlocks.includes( name )
+			  )
+			: [];
 	}, [ filteredItems, rootChildBlocks ] );
 
 	const suggestedItems = useMemo( () => {
