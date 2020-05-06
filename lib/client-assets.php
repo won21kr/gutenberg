52f201ec74fde5f7291ddeef7b932e398263b83b
Enrique Sánchez <enrique.sanchez.garcia@gmail.com>
2020-05-06 17:31:35 -0500

    Register pattern

--- lib/client-assets.php
+++ lib/client-assets.php
@@ -693,4 +693,5 @@ if ( class_exists( 'WP_Block_Patterns_Registry' ) && ! WP_Block_Patterns_Registr
 	register_block_pattern( 'core/numbered-features', gutenberg_load_block_pattern( 'numbered-features' ) );
 	register_block_pattern( 'core/its-time', gutenberg_load_block_pattern( 'its-time' ) );
 	register_block_pattern( 'core/testimonials', gutenberg_load_block_pattern( 'testimonials' ) );
+	register_block_pattern( 'core/pricing-table', gutenberg_load_block_pattern( 'pricing-table' ) );
 }
